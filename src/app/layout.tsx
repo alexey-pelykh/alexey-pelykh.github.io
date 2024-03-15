681959f92d927a48b65fd98b80f5193a7f8ee93a
Alexey Pelykh <alexey.pelykh@gmail.com>
2024-03-15 06:46:12 +0100

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from '@next/third-parties/google';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import * as React from "react";
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-QGWW798BRK" />
     </html>
   );
 }
