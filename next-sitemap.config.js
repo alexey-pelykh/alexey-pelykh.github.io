2375c3fc4216c380b29b8351ad4699b07248a0e9
Alexey Pelykh <alexey.pelykh@gmail.com>
2025-02-18 08:05:37 +0100

    (chore) re-allow other projects by robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,15 +3,6 @@ module.exports = {
   siteUrl: "https://alexey-pelykh.com",
   output: "export",
   generateRobotsTxt: true,
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: "*",
-        allow: "/",
-        disallow: ["/pcre4j/*", "/puppeteer-capture/*", "/UVCCamera/*"],
-      },
-    ],
-  },
   additionalPaths: async () => [{
     loc: "/resume.pdf",
   }],
